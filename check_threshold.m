0de218b4e414602fd87bd5e52795fc202567fc34
Neil Sharma <n.sharma777.ns@gmail.com>
2018-12-21 02:08:19 +0530

    Delete check_threshold.m

--- check_threshold.m
+++
@@ -1,9 +0,0 @@
-function check = check_threshold(check,threshold,m);
-%% converts the check matrix to a outlier detection matrix if (check>threshold)
- for i=1:m
-  if(check(i)>threshold)
-  check(i)=1;
-  else
-  check(i)=0;
-  end
-end
